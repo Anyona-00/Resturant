aafeaba780811ffc4aa5fadcd897a43275398fb1
Anyona-00 <125492744+Anyona-00@users.noreply.github.com>
2023-12-04 12:23:15 +0300

    complete project

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navLinks = document.getElementsByClassName('nav-links')[0]
 
-toggleButton.addEventListener('click', function() {
-    navLinks.classList.toggle('active')
+toggleButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    navLinks.classList.toggle('active');
+    toggleButton.classList.toggle('active');
 });
